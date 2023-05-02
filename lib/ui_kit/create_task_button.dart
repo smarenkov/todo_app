ea87b49726f9b75eff5be01f01e8a0215c11ec1c
Simon Marenkov <simon.marenkov@gmail.com>
2023-05-02 14:54:49 +0300

    Add onPressed VoidCallback to CreateTaskButton

--- lib/ui_kit/create_task_button.dart
+++ lib/ui_kit/create_task_button.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CreateTaskButton extends StatelessWidget {
-  const CreateTaskButton({super.key});
+  const CreateTaskButton({
+    required this.onPressed,
+    super.key,
+  });
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ButtonStyle(
         shape: MaterialStateProperty.all(const CircleBorder()),
         padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
